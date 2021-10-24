a42b4aa2715545be8d654b08f2b8ac1b89f71c79
shubham.kr08092000 <shubham.kr08092000@gmail.com>
2021-10-24 14:48:36 +0530

    first commit

---
+++ lib/sign_up_page.dart
@@ -0,0 +1,261 @@
+// ignore_for_file: unused_field
+
+import 'package:flutter/material.dart';
+import 'package:welcome/login.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePageLogin(),
+    );
+  }
+}
+
+class MyHomePageLogin extends StatefulWidget {
+  const MyHomePageLogin({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageLoginState createState() => _MyHomePageLoginState();
+}
+
+class _MyHomePageLoginState extends State<MyHomePageLogin> {
+  final _scaffoldkey = GlobalKey<ScaffoldState>();
+  final bool _loading = false;
+  final _formKey = GlobalKey<FormState>();
+  String _email = '', _password = '';
+  bool _obscuretext = true;
+
+  Widget _welcomeTitle() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height / 2,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/background-1.jpg'),
+              fit: BoxFit.fill,
+            ),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 20,
+                top: 150,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Track your',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontFamily: 'proxima',
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'project.',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontFamily: 'proxima',
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Get live updates',
+                      style: TextStyle(
+                        fontFamily: 'proxima',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _emailbox() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: TextFormField(
+        onSaved: (val) => _email = val!,
+        validator: (val) =>
+            RegExp(r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
+                    .hasMatch(val!)
+                ? null
+                : 'Invalid Email',
+        decoration: InputDecoration(
+          suffixIcon: GestureDetector(
+            child: const Icon(Icons.check),
+          ),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          labelText: "  Email",
+          labelStyle: const TextStyle(
+            fontFamily: 'proxima',
+            fontWeight: FontWeight.bold,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordbox() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: TextFormField(
+        onSaved: (val) => _password = val!,
+        obscureText: _obscuretext,
+        validator: (val) =>
+            val!.length < 6 ? "Minimum password length is 6 " : null,
+        decoration: InputDecoration(
+          suffixIcon: GestureDetector(
+            onTap: () {
+              setState(() => _obscuretext = !_obscuretext);
+            },
+            child: Icon(_obscuretext ? Icons.visibility : Icons.visibility_off),
+          ),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          labelText: "  Password",
+          labelStyle: const TextStyle(
+            fontFamily: 'proxima',
+            fontWeight: FontWeight.bold,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _confirmpasswordbox() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: TextFormField(
+        onSaved: (val) => _password = val!,
+        obscureText: _obscuretext,
+        validator: (val) =>
+            val!.length < 6 ? "Minimum password length is 6 " : null,
+        decoration: InputDecoration(
+          suffixIcon: GestureDetector(
+            onTap: () {
+              setState(() => _obscuretext = !_obscuretext);
+            },
+            child: Icon(_obscuretext ? Icons.visibility : Icons.visibility_off),
+          ),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          labelText: "  Confirm  Password",
+          labelStyle: const TextStyle(
+            fontFamily: 'proxima',
+            fontWeight: FontWeight.bold,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _signUpButton() {
+    return Column(
+      children: [
+        _loading == true
+            ? const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(
+                  Color.fromRGBO(79, 70, 229, 1),
+                ),
+              )
+            : GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute<void>(
+                      builder: (BuildContext context) => const LoginPage(),
+                    ),
+                  );
+                },
+                child: SizedBox(
+                  height: 40.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(8.0),
+                    shadowColor: const Color.fromRGBO(79, 70, 229, 1),
+                    color: const Color.fromRGBO(79, 70, 229, 1),
+                    elevation: 7.0,
+                    child: const Center(
+                      child: Text(
+                        "SIGN UP",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'proxima',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+      ],
+    );
+  }
+
+  // Main Part Widget
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldkey,
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              _welcomeTitle(),
+              const Padding(
+                padding: EdgeInsets.only(left: 20, top: 20.0),
+                child: Text(
+                  ' Sign Up',
+                  style: TextStyle(
+                    fontFamily: 'proxima',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Form(
+                key: _formKey,
+                child: Container(
+                  padding:
+                      const EdgeInsets.only(top: 8.0, left: 20.0, right: 20.0),
+                  child: Column(
+                    children: <Widget>[
+                      _emailbox(),
+                      _passwordbox(),
+                      _confirmpasswordbox(),
+                      const SizedBox(height: 30.0),
+                      _signUpButton(),
+                      const SizedBox(height: 30.0),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 35.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:welcome/login.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SplashScreenState();
+  }
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background-1.jpg'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 80,
+              child: Column(
+                children: [
+                  const Text(
+                    'Welcome',
+                    style: TextStyle(
+                      fontSize: 45.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'proxima',
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Track your project progress',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'proxima',
+                      color: Colors.grey.shade700,
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.68),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 30),
+                    width: MediaQuery.of(context).size.width,
+                    child: _loginButton(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute<void>(
+            builder: (BuildContext context) => const LoginPage(),
+          ),
+        );
+      },
+      child: SizedBox(
+        height: 40.0,
+        child: Material(
+          borderRadius: BorderRadius.circular(8.0),
+          shadowColor: const Color.fromRGBO(79, 70, 229, 1),
+          color: const Color.fromRGBO(79, 70, 229, 1.0),
+          elevation: 7.0,
+          child: const Center(
+            child: Text(
+              "Get Started",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'proxima',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
